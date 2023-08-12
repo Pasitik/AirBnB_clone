bab3a93cb89b20639c400a12e2dbc4f1187234d8
Paul Asitik <paasitik@gmail.com>
2023-08-12 09:54:52 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-""" A class BaseModel that defines all common attributes/methods for other classes:
+""" A class BaseModel that defines all common
+    attributes/methods for other classes:
 """
 from uuid import uuid4
 from datetime import datetime
 from sys import path
-path.append('.')
 from models import storage
+path.append('.')
+
 
 class BaseModel:
     """ Defines the base model class"""
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             if "__class__" in kwargs:
                 del kwargs["__class__"]
             for key, value in kwargs.items():
                 if (key == "created_at" or key == "updated_at"):
-                    att_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    att_value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, att_value)
                 else:
                     setattr(self, key, value)
@@ -24,7 +28,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-    
+
     def save(self):
         """Saves an instance to a file """
         self.updated_at = datetime.now()
@@ -35,14 +39,14 @@ class BaseModel:
         """Resolves an instance to a ditionary """
         dictionary = {}
         dictionary["__class__"] = self.__class__.__name__
-        for key,value in self.__dict__.items():
+        for key, value in self.__dict__.items():
             if isinstance(value, datetime):
                 dictionary[key] = value.isoformat()
             else:
                 dictionary[key] = value
 
         return dictionary
-    
+
     def __str__(self):
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
