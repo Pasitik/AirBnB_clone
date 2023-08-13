a91c3eb5f756b40403da60eb7f442634d9474e2f
DD1C74707 <gracevalley101@gmail.com>
2023-08-13 15:48:05 -0700

    Fix amenity name

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -56,5 +56,31 @@ class TestCity(unittest.TestCase):
         self.assertIn(str(city.__dict__), str_repr)
 
 
+class TestCityPlus(unittest.TestCase):
+    """Test City class"""
+
+    def test_attribut_state_id(self):
+        """ Test with state id"""
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        self.assertEqual(city.state_id, "")
+        self.assertTrue(type(city.state_id) == str)
+
+    def test_attribut_name(self):
+        """ Test with state name"""
+        city = City()
+        self.assertTrue(hasattr(city, "name"))
+        self.assertEqual(city.name, "")
+        self.assertTrue(type(city.name) == str)
+
+    def test_class_doc(self):
+        """ ``City`` documentation"""
+        self.assertIsNotNone(City.__doc__)
+
+    def test_module_doc(self):
+        """ Test the city_model module's documentation"""
+        self.assertIsNotNone(models.city.__doc__)
+
+
 if __name__ == "__main__":
     unittest.main()
