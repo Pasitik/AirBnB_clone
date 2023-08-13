3a417e2c1111b1f6c5644e67433ae6a691fef49e
DD1C74707 <gracevalley101@gmail.com>
2023-08-13 13:18:34 -0700

    Add console test

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Test module for console module """
+
+
+from console import HBNBCommand
+import unittest
+from datetime import datetime
+import io
+import sys
+
+
+class TestConsole(unittest.TestCase):
+    """ A TestCase class that tests the BaseModel class """
+
+    pass
